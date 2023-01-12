52c97847da49b652c6e1a8c77e55e28a492e04a5
Furkan Bilgi <bilgifurkan3@gmail.com>
2023-01-12 13:32:04 +0300

    .

--- Task7/Assets/Scripts/Collectable.cs
+++ Task7/Assets/Scripts/Collectable.cs
@@ -8,7 +8,7 @@ public class Collectable : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            PlayerConroller.instance.score += 5;
+            PlayerConroller.instance.score += 10;
             PlayerConroller.instance.scoreText.text = PlayerConroller.instance.score.ToString();
             PlayerConroller.instance.bigPlayer.transform.localScale = new Vector3(PlayerConroller.instance.bigPlayer.transform.localScale.x + .15f, PlayerConroller.instance.bigPlayer.transform.localScale.y + .15f, PlayerConroller.instance.bigPlayer.transform.localScale.z + .15f);
             Destroy(gameObject);
